d89ae4c7bbdeace248931b3f0b0b15af92b2243a
Piyush Sharma <piyushsharma220699@gmail.com>
2023-02-22 20:49:43 +0530

    Added the CRUD APIs of Django-Strawberry for Blog API

--- blog/api/schema.py
+++ blog/api/schema.py
@@ -25,14 +25,34 @@ class BlogInput:
     updated_at: strawberry.auto
 
 
+@strawberry.django.filters.filter(models.Blog)
+class BlogFilter:
+    id: strawberry.auto
+
+
+@strawberry_django.input(models.Blog)
+class BlogPartialFilter(BlogInput):
+    pass
+
+
 @strawberry.type
 class Query:
     getAllBlogs: typing.List[Blog] = strawberry.django.field()
+    getBlogByID: Blog = strawberry.django.field()
+    # @strawberry.field
+    # def get_blog(self, info, blog_id: int) -> Blog:
+    #     try:
+    #         blog = models.Blog.objects.get(pk=blog_id)
+    #         return blog
+    #     except models.Blog.DoesNotExist:
+    #         raise ValueError(f"Blog with ID {blog_id} does not exist.")
 
 
 @strawberry.type
 class Mutation:
     createBlog: Blog = mutations.create(BlogInput)
+    deleteBlogById: typing.List[Blog] = mutations.delete(filters=BlogFilter)
+    updateBlogByID: typing.List[Blog] = mutations.update(BlogPartialFilter, filters=BlogFilter)
 
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
@@ -63,5 +83,41 @@ schema = strawberry.Schema(query=Query, mutation=Mutation)
 #     author
 #     createdAt
 #     updatedAt
+#   }
+# }
+
+# GET BLOG BY ID
+# query {
+#   getBlogByID(pk:1) {
+#     id
+#     title
+#     author
+#   }
+# }
+
+# DELETE BLOG BY ID
+# mutation{
+#   deleteBlogById(filters:{
+#     id:1
+#   })
+#   {
+#     id
+#     title
+#     content
+#   }
+# }
+
+# UPDATE BY ID
+# mutation{
+#   updateBlogByID(filters: {
+#     id: 2
+#   }data: {
+#     title: "Hello Piyush"
+#     content: "We did it"
+#   }){
+#     id
+#     content
+#     title
+#     author
 #   }
 # }
